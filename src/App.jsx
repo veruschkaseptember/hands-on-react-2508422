c1b1f59562b7bd44e72674e808c13033e03cd778
Veruschka Natasja September <veruschkaseptember@yahoo.com>
2022-11-30 10:01:54 +0000

    3 Test changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <div className="container">
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the Stargazers</h1>
+          <h1>Meet the Starz</h1>
           <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
           {memberInfo && <Modals member={memberInfo} handleChange={(info) => { setMemberInfo(cast[info]) }} handleClose={() => { setMemberInfo(null) }} />}
